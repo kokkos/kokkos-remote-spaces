675c691a164314591721b31a76d73267c25c28af
Christian Trott <crtrott@sandia.gov>
2017-12-09 10:58:46 -0700

    Fix symmetric view example

--- examples/symmetric_view/main.cpp
+++ examples/symmetric_view/main.cpp
@@ -21,21 +21,26 @@ int main(int argc, char* argv[]) {
     rank_list[1] = 1;
     view_type a = 
       Kokkos::allocate_symmetric_remote_view<view_type>("A",num_ranks,rank_list,10);
-    int* data = a.data(); 
-    printf("Ptr: %i %p\n",my_rank,data);
+    space.fence();
+
     {
       Kokkos::View<int**, Kokkos::QUOSpace> a_1 = a;
       printf("Refcount a_1: %i Rank: %i\n",a_1.use_count(),my_rank);
+      if(a_1.use_count()!=2) printf("Error RefCount %i %i\n",my_rank,a_1.use_count());
     }
+    if(a.use_count()!=1) printf("Error RefCount %i %i\n",my_rank,a.use_count());
+
     for(int i = 0; i < 10; i++)
       a(my_rank,i) = (my_rank + 1) * 100 + i%10;
-    printf("Refcount a: %i Rank: %i\n",a.use_count(),my_rank);
     space.fence();
 
     for(int rank=0; rank<num_ranks ; rank++)
-      for(int i=0; i<10; i++)
-        printf("%i %i %i %i Data\n",my_rank*10000+rank*100+i,my_rank,i,a(rank,i));  
-    
+      for(int i=0; i<10; i++) {
+        if(i==0)
+          printf("%i %i %i %i %i Data\n",my_rank*10000+rank*100+i,my_rank,rank,i,a(rank,i));  
+        if(a(rank,i) != ((rank+1)*100 + i%10))     
+          printf("Error Data: %i %i %i %i %i\n",my_rank,rank,i,a(rank,i),((rank + 1)*100 + i%10));
+      }
   }
   Kokkos::finalize();
   MPI_Finalize();
