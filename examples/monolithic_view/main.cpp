8cbd48fd8f8dd8ec6d8b3d7303fe5eaa1aac51a4
Christian Trott <crtrott@sandia.gov>
2017-12-09 10:42:40 -0700

    Fix example for monolithic view

--- examples/monolithic_view/main.cpp
+++ examples/monolithic_view/main.cpp
@@ -13,20 +13,27 @@ int main(int argc, char* argv[]) {
 
   Kokkos::QUOSpace  space;
   {
-    Kokkos::View<int*, Kokkos::QUOSpace> a("A",num_ranks * 10 * sizeof(int));  
-    int* data = a.data(); 
-    printf("Ptr: %i %p\n",my_rank,data);
+    Kokkos::View<int*, Kokkos::QUOSpace> a("A",num_ranks * 10 * sizeof(int)); 
+    // This should not be needed 
+    space.fence();
+    
     {
       Kokkos::View<int*, Kokkos::QUOSpace> a_1 = a;
       printf("Refcount a_1: %i Rank: %i\n",a_1.use_count(),my_rank);
+      if(a_1.use_count()!=2) printf("Error RefCount %i %i\n",my_rank,a_1.use_count());
     }
+    if(a.use_count()!=1) printf("Error RefCount %i %i\n",my_rank,a.use_count());
+    
     for(int i = my_rank * 10; i < (my_rank+1) * 10; i++)
       a(i) = (my_rank + 1) * 100 + i%10;
-    printf("Refcount a: %i Rank: %i\n",a.use_count(),my_rank);
+
     space.fence();
 
-    for(int i=0; i<num_ranks * 10; i++)
-      printf("%i %i %i %i Data\n",my_rank*10000+i,my_rank,i,a(i));  
+    for(int i=0; i<num_ranks * 10; i++) {
+      if(i%10==0)
+        printf("%i %i %i %i Data\n",my_rank*10000+i,my_rank,i,a(i));  
+      if(a(i) != ((i/10 + 1)*100 + i%10)) printf("Error Data: %i %i %i %i\n",my_rank,i,a(i),((i/10 + 1)*100 + i%10));
+    }
   }
   Kokkos::finalize();
   MPI_Finalize();
