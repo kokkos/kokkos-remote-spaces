dcb90650c7ce12a3079adefd51918cbff4d99722
Jan Ciesko <jan.ciesko@gmail.com>
2022-08-01 10:35:02 -0600

    Move setting alloc_name into if-body

--- src/core/Kokkos_RemoteSpaces_ViewMapping.hpp
+++ src/core/Kokkos_RemoteSpaces_ViewMapping.hpp
@@ -1204,14 +1204,14 @@ class ViewMapping<Traits, Kokkos::Experimental::RemoteSpaceSpecializeTag> {
     }
 #endif
 
-    const std::string &alloc_name =
-        static_cast<Kokkos::Impl::ViewCtorProp<void, std::string> const &>(
-            arg_prop)
-            .value;
-
     //  Only initialize if the allocation is non-zero.
     //  May be zero if one of the dimensions is zero.
     if (alloc_size && alloc_prop::initialize) {
+      const std::string &alloc_name =
+          static_cast<Kokkos::Impl::ViewCtorProp<void, std::string> const &>(
+              arg_prop)
+              .value;
+
       // Assume destruction is only required when construction is requested.
       // The ViewValueFunctor has both value construction and destruction
       // operators.
