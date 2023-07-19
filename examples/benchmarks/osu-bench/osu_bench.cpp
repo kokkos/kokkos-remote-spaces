0d77906c8081a8135da64acc23ce1f0ed52d0f7d
Danny <dsmishler@gmail.com>
2023-07-19 11:53:45 -0600

    templated puts and gets for less code duplication. Fixed latency benchmark as well.
    Can do both benchmarks at once.

--- examples/benchmarks/osu-bench/osu_bench.cpp
+++ examples/benchmarks/osu-bench/osu_bench.cpp
@@ -2,32 +2,39 @@
 #include <Kokkos_RemoteSpaces.hpp>
 #include <mpi.h>
 
-using RemoteSpace_t      = Kokkos::Experimental::DefaultRemoteMemorySpace;
-using RemoteDoubleView_t = Kokkos::View<double *, RemoteSpace_t>;
+using RemoteSpace_t = Kokkos::Experimental::DefaultRemoteMemorySpace;
+/* allows avoiding of code duplication */
+#define LATENCY_BIT 1
+#define MESSAGE_RATE_BIT 2
+#define BOTH_BITS LATENCY_BIT | MESSAGE_RATE_BIT
 
-template <typename ViewType_t>
+template <typename dType>
 struct Bench_Manager {
-  ViewType_t X;
+  using RemoteView_t = Kokkos::View<dType *, RemoteSpace_t>;
+  RemoteView_t X;
   int len = 0;
   int me;
   int partner;
   int nranks;
   int my_start_i;
   int partner_start_i;
-  int interval;
+  int interval; /* all processes always have the same interval */
   int iterations;
   int t;
+  int dType_size;
 
   double start_time, end_time, benchmark_time;
 
   Bench_Manager(int l, int iters) : len(l), iterations(iters) {
-    X = ViewType_t("Device::X", len);
+    X = RemoteView_t("Device::X", len);
     MPI_Comm_rank(MPI_COMM_WORLD, &me);
     MPI_Comm_size(MPI_COMM_WORLD, &nranks);
     partner         = (me + (nranks / 2)) % nranks;
     interval        = len / nranks;
     my_start_i      = me * interval;
     partner_start_i = partner * interval;
+
+    dType_size = sizeof(dType);
 
     printf("I am rank %d with access to [%d,%d) and my partner is %d\n", me,
            my_start_i, my_start_i + interval, partner);
@@ -39,8 +46,6 @@ struct Bench_Manager {
   using policy_get_t = Kokkos::RangePolicy<b_get, size_t>;
   KOKKOS_FUNCTION
   void operator()(const b_put &, const size_t i) const {
-    // printf("copying X(%d) to X(%d)\n", (int)(my_start_i+i),
-    // (int)(partner_start_i+i));
     X(partner_start_i + i) = X(my_start_i + i);
   }
   KOKKOS_FUNCTION
@@ -48,13 +53,13 @@ struct Bench_Manager {
     X(my_start_i + i) = X(partner_start_i + i);
   }
 
-  void benchmark_put_lat(void) {
-    Kokkos::Timer timer;  // TODO: how come this can't go up top?
+  template <typename policy_t>
+  void benchmark(int btype_num) {
+    Kokkos::Timer timer;
     benchmark_time = 0.0;
     if (me < nranks / 2) {
-      Kokkos::parallel_for("KOKKOS::put_lat WARMUP",
-                           policy_put_t(my_start_i, my_start_i + interval),
-                           *this);
+      Kokkos::parallel_for("KOKKOS::bench WARMUP",
+                           policy_t(my_start_i, my_start_i + interval), *this);
     }
     RemoteSpace_t().fence();
     Kokkos::fence();
@@ -62,8 +67,8 @@ struct Bench_Manager {
       if (t % 100 == 0 && me == 0) printf("iteration %d\n", t);
       start_time = timer.seconds();
       if (me < nranks / 2) {
-        Kokkos::parallel_for("KOKKOS::put_lat",
-                             policy_put_t(my_start_i, my_start_i + interval),
+        Kokkos::parallel_for("KOKKOS::bench",
+                             policy_t(my_start_i, my_start_i + interval),
                              *this);
       }
       RemoteSpace_t().fence();
@@ -71,83 +76,35 @@ struct Bench_Manager {
       end_time = timer.seconds();
       benchmark_time += (end_time - start_time);
     }
-    if (me == 0) printf("total benchmark time: %lf seconds\n", benchmark_time);
-  }
-  void benchmark_get_lat(void) {
-    Kokkos::Timer timer;  // TODO: how come this can't go up top?
-    benchmark_time = 0.0;
-    if (me < nranks / 2) {
-      Kokkos::parallel_for("KOKKOS::get_lat WARMUP",
-                           policy_get_t(my_start_i, my_start_i + interval),
-                           *this);
-    }
-    RemoteSpace_t().fence();
-    Kokkos::fence();
-    for (t = 1; t <= iterations; t++) {
-      if (t % 100 == 0 && me == 0) printf("iteration %d\n", t);
-      start_time = timer.seconds();
-      if (me < nranks / 2) {
-        Kokkos::parallel_for("KOKKOS::get_lat",
-                             policy_get_t(my_start_i, my_start_i + interval),
-                             *this);
+    if (me == 0) {
+      if (std::is_same<policy_t, policy_put_t>::value) {
+        printf("operation: PUT\n");
       }
-      RemoteSpace_t().fence();
-      Kokkos::fence();
-      end_time = timer.seconds();
-      benchmark_time += (end_time - start_time);
-    }
-    if (me == 0) printf("total benchmark time: %lf seconds\n", benchmark_time);
-  }
-  void benchmark_put_mr(void) {
-    Kokkos::Timer timer;  // TODO: how come this can't go up top?
-    benchmark_time = 0.0;
-    if (me < nranks / 2) {
-      Kokkos::parallel_for("KOKKOS::put_mr WARMUP",
-                           policy_put_t(my_start_i, my_start_i + interval),
-                           *this);
-    }
-    RemoteSpace_t().fence();
-    Kokkos::fence();
-    for (t = 1; t <= iterations; t++) {
-      if (t % 100 == 0 && me == 0) printf("iteration %d\n", t);
-      start_time = timer.seconds();
-      if (me < nranks / 2) {
-        Kokkos::parallel_for("KOKKOS::put_mr",
-                             policy_put_t(my_start_i, my_start_i + interval),
-                             *this);
+      if (std::is_same<policy_t, policy_get_t>::value) {
+        printf("operation: GET\n");
       }
-      RemoteSpace_t().fence();
-      Kokkos::fence();
-      end_time = timer.seconds();
-      benchmark_time += (end_time - start_time);
-    }
-    if (me == 0) printf("total benchmark time: %lf seconds\n", benchmark_time);
-  }
-  void benchmark_get_mr(void) {
-    Kokkos::Timer timer;  // TODO: how come this can't go up top?
-    benchmark_time = 0.0;
-    if (me < nranks / 2) {
-      Kokkos::parallel_for("KOKKOS::get_mr WARMUP",
-                           policy_get_t(my_start_i, my_start_i + interval),
-                           *this);
-    }
-    RemoteSpace_t().fence();
-    Kokkos::fence();
-    for (t = 1; t <= iterations; t++) {
-      if (t % 100 == 0 && me == 0) printf("iteration %d\n", t);
-      start_time = timer.seconds();
-      if (me < nranks / 2) {
-        Kokkos::parallel_for("KOKKOS::get_mr",
-                             policy_get_t(my_start_i, my_start_i + interval),
-                             *this);
+
+      double message_bytes = 1.0 * interval * dType_size * iterations;
+      printf("dtype_bytes %d message_bytes %.0lf\n", dType_size, message_bytes);
+      printf("total benchmark time: %lf seconds\n", benchmark_time);
+
+      if (btype_num & LATENCY_BIT) {
+        double avg_time_us = benchmark_time / iterations * 1e6;
+        printf("latency: %lf (microseconds)\n", avg_time_us);
       }
-      RemoteSpace_t().fence();
-      Kokkos::fence();
-      end_time = timer.seconds();
-      benchmark_time += (end_time - start_time);
+      if (btype_num & MESSAGE_RATE_BIT) {
+        double message_gbytes_per_sec = message_bytes * 1e-9 / benchmark_time;
+        printf("bandwidth: %lf (GB/s)\n", message_gbytes_per_sec);
+      }
     }
-    if (me == 0) printf("total benchmark time: %lf seconds\n", benchmark_time);
   }
+
+  void benchmark_put_lat(void) { benchmark<policy_put_t>(LATENCY_BIT); }
+  void benchmark_get_lat(void) { benchmark<policy_get_t>(LATENCY_BIT); }
+  void benchmark_put_mr(void) { benchmark<policy_put_t>(MESSAGE_RATE_BIT); }
+  void benchmark_get_mr(void) { benchmark<policy_get_t>(MESSAGE_RATE_BIT); }
+  void benchmark_put_all(void) { benchmark<policy_put_t>(BOTH_BITS); }
+  void benchmark_get_all(void) { benchmark<policy_get_t>(BOTH_BITS); }
 };
 
 int main(int argc, char *argv[]) {
@@ -177,9 +134,9 @@ int main(int argc, char *argv[]) {
 
   Kokkos::initialize(argc, argv);
   {
-    int mode  = 0;
-    int len   = 1e4;
-    int iters = 1e4;
+    int mode  = 4;
+    int len   = 1e6;
+    int iters = 1e3;
     for (int i = 1; i < argc; i++) {
       if (strcmp(argv[i], "-h") == 0) {
         printf("  -m IARG: (%d) which mode to choose\n", mode);
@@ -190,11 +147,15 @@ int main(int argc, char *argv[]) {
         printf("  1: osu get latency\n");
         printf("  2: osu put message rate\n");
         printf("  3: osu get message rate\n");
+        printf("  4: osu put both benchmarks\n");
+        printf("  5: osu get both benchmarks\n");
         break;
       }
       if (strcmp(argv[i], "-m") == 0)
         mode = atoi(argv[i + 1]);
       else if (strcmp(argv[i], "-l") == 0)
+        len = atoi(argv[i + 1]);
+      else if (strcmp(argv[i], "-N") == 0)
         len = atoi(argv[i + 1]);
       else if (strcmp(argv[i], "-i") == 0)
         iters = atoi(argv[i + 1]);
@@ -211,7 +172,8 @@ int main(int argc, char *argv[]) {
       exit(1);
     }
 
-    Bench_Manager<RemoteDoubleView_t> sys(len, iters);
+    /* different dtypes are not supported yet, but easily could be extended. */
+    Bench_Manager<double> sys(len, iters);
     if (mode == 0) {
       sys.benchmark_put_lat();
     } else if (mode == 1) {
@@ -220,6 +182,10 @@ int main(int argc, char *argv[]) {
       sys.benchmark_put_mr();
     } else if (mode == 3) {
       sys.benchmark_get_mr();
+    } else if (mode == 4) {
+      sys.benchmark_put_all();
+    } else if (mode == 5) {
+      sys.benchmark_get_all();
     } else {
       printf("invalid mode selected (%d)\n", mode);
     }
